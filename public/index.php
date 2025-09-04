b79abb21220a864c2e3f11f3b516d430d63a5003
Hichxm <hicham.slimani.fr@gmail.com>
2025-09-04 21:44:59 +0200

    Fix routing logic in index.php to include app bootstrap only for non-static requests

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 if (preg_match('/\.(?:js|css|jpg|woff|woff2|otf|eot|svg|ttf)$/', $_SERVER['REQUEST_URI'])) {
     return false; // serve the requested resource as-is.
+} else {
+    require_once __DIR__ . '/../framework/app.php';
 }
 
-require_once __DIR__ . '/../framework/app.php';
