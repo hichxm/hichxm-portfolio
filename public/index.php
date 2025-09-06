124bef5c2eef03ad00170501281c5172651e307f
Hichxm <hicham.slimani.fr@gmail.com>
2025-09-06 02:48:40 +0200

    Adjust static file handling in `index.php` for improved clarity and maintainability

--- public/index.php
+++ public/index.php
@@ -1,7 +1,9 @@
 <?php
 
 if (preg_match('/\.(?:js|css|jpg|png|webp|woff|woff2|otf|eot|svg|ttf)$/', $_SERVER['REQUEST_URI'])) {
-    return false; // serve the requested resource as-is.
+    // serve the requested resource as-is.
+
+    return false;
 } else {
     require_once __DIR__ . '/../framework/app.php';
 }
