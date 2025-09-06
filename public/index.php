0796d67d0f3ba672d9aa77e7eea5930cb7b66c64
Hichxm <hicham.slimani.fr@gmail.com>
2025-09-06 03:23:24 +0200

    Add `pdf` to handled static file extensions in `index.php`

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-if (preg_match('/\.(?:js|css|jpg|png|webp|woff|woff2|otf|eot|svg|ttf)$/', $_SERVER['REQUEST_URI'])) {
+if (preg_match('/\.(?:js|css|jpg|png|webp|pdf|woff|woff2|otf|eot|svg|ttf)$/', $_SERVER['REQUEST_URI'])) {
     // serve the requested resource as-is.
 
     return false;
